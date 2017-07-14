6077327cd74acb61b59ac4dba8cae9239b5f3e1d
Taylor Cox <taylorashtoncox@gmail.com>
2017-07-14 12:54:15 -0400

    Changed divs to buttons in HTML, restyled buttons in CSS, added click visuals in JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
+// Variables declared for calculator
+const zero = document.querySelector('#zero')
+const one = document.querySelector('#one')
+const two = document.querySelector('#two')
+const three = document.querySelector('#three')
+const four = document.querySelector('#four')
+const five = document.querySelector('#five')
+const six = document.querySelector('#six')
+const seven = document.querySelector('#seven')
+const eight = document.querySelector('#eight')
+const nine = document.querySelector('#nine')
+const add = document.querySelector('#add')
+const subtract = document.querySelector('#subtract')
+const multiply = document.querySelector('#multiply')
+const divide = document.querySelector('#divide')
+const equals = document.querySelector('#equals')
+const clear = document.querySelector('#clear')
+const operator = document.querySelectorAll('.operator')
+const results = document.querySelector('.results-box')
+
+
+// Click Effects for Clear and Equals Buttons
+clear.addEventListener("mousedown", function(){
+  clear.className = "btn-click"
+})
+
+equals.addEventListener("mousedown", function(){
+  equals.className = "btn-click"
+})
+
+clear.addEventListener("mouseup", function(){
+  clear.className = ""
+})
+
+equals.addEventListener("mouseup", function(){
+  equals.className = ""
+})
