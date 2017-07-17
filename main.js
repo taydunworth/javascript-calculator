13c8e1c587012645a3ef4351b5beff3afe7feef9
Taylor Cox <taylorashtoncox@gmail.com>
2017-07-17 14:25:12 -0400

    Scratched original logic JavaScript and cleaned up DOM manipulation

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
 // Variables declared for calculator
-const zero = document.querySelector('#zero')
-const one = document.querySelector('#one')
-const two = document.querySelector('#two')
-const three = document.querySelector('#three')
-const four = document.querySelector('#four')
-const five = document.querySelector('#five')
-const six = document.querySelector('#six')
-const seven = document.querySelector('#seven')
-const eight = document.querySelector('#eight')
-const nine = document.querySelector('#nine')
-const add = document.querySelector('#add')
-const subtract = document.querySelector('#subtract')
-const multiply = document.querySelector('#multiply')
-const divide = document.querySelector('#divide')
-const decimal = document.querySelector('#decimal')
-const equals = document.querySelector('#equals')
-const clear = document.querySelector('#clear')
-const operator = document.querySelectorAll('.operator')
+const clear = document.getElementById('clear')
+const operator = document.querySelectorAll('button.operator')
 const resultsBox = document.querySelector('.results-box')
 const results = document.querySelector('.results')
-const buttons = document.querySelectorAll('button')
-const value = document.querySelectorAll("button").value
+const number = document.querySelectorAll('button.number')
+const equals = document.querySelector('#equals')
 
 
 // Click Effects for Clear and Equals Buttons
@@ -42,53 +26,42 @@ equals.addEventListener("mouseup", function() {
 
 // Logic
 
-  function calculator() {
+// set variables for first and second numbers
+let numOne = 0
+let numTwo = 0
 
-    // Buttons appending to results box
+number.forEach(number => {
+  number.addEventListener("click", numberEvent => {
+    let numberValue = numberEvent.target.value
+    results.textContent += numberValue
+  })
+})
 
-    const resultsArray = Array.from(results)
-    let length = buttons.length
+operator.forEach(operator => {
+  operator.addEventListener("click", operatorEvent => {
+    let operatorValue = operatorEvent.target.value
+    results.textContent += operatorValue
+  })
+})
 
-    // Clear results "C" is clicked
-    for (i = 0; i < length; i++) {
-      let value = buttons[i].value
-      if (value == "c") {
-        buttons[i].addEventListener("mousedown", function() {
-          results.textContent= " "
-        })
+clear.forEach(clear => {
+  clear.addEventListener("click", clearEvent =>  {
+    results.textContent = ""
+  })
+})
 
-      // Adds number value when button is clicked
-    } else if (value != "=" && value != "+" && value!= "-" && value != "/" && value != "*") {
-        buttons[i].addEventListener("mousedown", function() {
-          num = this.value
-          results.append(parseInt(num, 10))
-          // need to store values somewhere to recall for equation
-        })
-
-      // Adds operator when button is clicked and does not allow the "=" to append
-    } else if (value != "="){
-        buttons[i].addEventListener("mousedown", function() {
-          num = this.value
-          results.append(value)
-      })
-      }
-    }
-
-    // Do the actual equations when "=" is clicked based on if the operator exists
-
-    for (i = 0; i < length; i++) {
-      if (buttons[i].value == "=") {
-        if (results.includes("+")) {
-          num += num
-        } else if (results.includes("-")) {
-          num -= num
-        } else if (results.includes("/")) {
-          num /= num
-        } else if (results.includes("*")) {
-          num *= num
-        }
-      }
-    }
+equals.addEventListener("click", answerEvent => {
+  if (operation.value === "+") {
+    results.textContent = numOne + numTwo
+  } else if (operation.value === '-') {
+    results.textContent = numOne - numTwo
+  } else if (operation.value === '/') {
+    results.textContent = numOne / numTwo
+  } else if (operation.value === '*') {
+    results.textContent = numOne * numTwo
   }
+})
 
-calculator()
+numOne = ''
+numTwo = ''
+operator = ''
